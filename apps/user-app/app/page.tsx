2264f0f171a537ffdbcca4c013f9719b4c55c641
al1211 <anil.kumar0372915@gmail.com>
2026-07-05 11:05:42 +0530

    add package db and recoil

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,6 +1,9 @@
 import Image, { type ImageProps } from "next/image";
 import { Button } from "@repo/ui/button";
 import styles from "./page.module.css";
+import {prisma} from "@repo/db"
+
+
 
 type Props = Omit<ImageProps, "src"> & {
   srcLight: string;
---
+++ packages/store/src/useBalance.ts
@@ -0,0 +1,9 @@
+
+import { useRecoilValue } from "recoil";
+import { balanceAtom } from "./balance";
+
+
+export const useBalance=()=>{
+    const value=useRecoilValue(balanceAtom);
+    return value
+}
