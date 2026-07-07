da29ed9140f1e313d6a047e2ba725add354297bf
al1211 <anil.kumar0372915@gmail.com>
2026-07-07 12:07:53 +0530

    solve env problem in my db package

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -1,18 +1,17 @@
 import dotenv from "dotenv";
 import path from "path";
 
+// Resolves path relative to the running application (apps/user-app)
 dotenv.config({
-  path: path.join(__dirname, ".env"),
+  path: path.resolve(process.cwd(), "../../packages/db/.env")
 });
-import { PrismaPg } from "@prisma/adapter-pg"
+
+import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "./generated/prisma/client";
 
-const connectionString=`${process.env.DATABASE_URL}`
+const connectionString = `${process.env.DATABASE_URL}`;
+const adapter = new PrismaPg({ connectionString });
 
-const adapter=new PrismaPg({connectionString});
+export const prisma = new PrismaClient({ adapter });
 
-export const prisma=new PrismaClient({adapter});
-console.log(process.env.DATABASE_URL)
-console.log("__dirname:", __dirname);
-console.log("cwd:", process.cwd());
-
+console.log("Database URL:", process.env.DATABASE_URL);
