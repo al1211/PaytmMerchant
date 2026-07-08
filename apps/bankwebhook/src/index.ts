db8b68565fe58a46cd599e66191a935df8a321f0
al1211 <anil.kumar0372915@gmail.com>
2026-07-08 22:09:15 +0530

    fix my tailwind css and add component

--- apps/bankwebhook/src/index.ts
+++ apps/bankwebhook/src/index.ts
@@ -1,12 +1,54 @@
 import express, { Request, Response } from "express"
-
+import {prisma} from "@repo/db"
 const app=express();
 
 
 app.post("/hdfcbank",async(req:Request,res:Response)=>{
-  const paymentInformation={
+  const paymentInformation:{
+    token:string,
+    userId:string,
+    amount:string
+  }={
     token:req.body.token,
     userId:req.body.user_identifier,
     amount:req.body.amount
+  };
+  try{
+    
+    await prisma.$transaction([
+
+       prisma.balance.updateMany({
+        where:{
+          userId:Number(paymentInformation.userId)
+        },
+        data:{
+          amount:{
+            increment:Number(paymentInformation.amount)
+          }
+        }
+      }),
+       prisma.onRampTransaction.updateMany({
+        where:{
+          token:paymentInformation.token
+        },
+        data:{
+          status:"Success"
+        }
+      })
+    ]);
+
+  }catch(e){
+    console.error(e);
+    res.status(411).json({
+      message:"Error while processing webhook"
+        })
   }
+ 
+
+        res.json({
+            message: "Captured"
+        })
+
 })
+
+app.listen(3003);
--- apps/user-app/app/layout.tsx
+++ apps/user-app/app/layout.tsx
@@ -2,6 +2,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+
 import Providers from "../Provider";
 import {Inter} from "next/font/google"
 import { AppbarClient } from "../component/AppbarClent";
